acc9c7f2a4ef55289be70b907d92817439108b4e
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-11-18 20:58:42 +0530

    UI Activated

--- My Heart Pal/chatbot.py
+++ My Heart Pal/chatbot.py
@@ -1,5 +1,6 @@
+# chatbot.py
 import tkinter as tk
-from tkinter import Text, Scrollbar, Entry, Button, END
+from tkinter import Text, Entry, Button, END
 import random
 import json
 import pickle
@@ -8,65 +9,66 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 
-lemmatizer = WordNetLemmatizer()
+class Chatbot:
+    def __init__(self):
+        self.lemmatizer = WordNetLemmatizer()
+        self.words = pickle.load(open('words.pkl', 'rb'))
+        self.classes = pickle.load(open('classes.pkl', 'rb'))
+        self.model = load_model('chatbot_model.h5')
+        
+        try:
+            with open('intents.json', 'r') as file:
+                self.intents = json.load(file)
+        except FileNotFoundError:
+            print("The file 'intents.json' was not found.")
+            self.intents = None
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            self.intents = None
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            self.intents = None
 
-# Load intents from the JSON file
-try:
-    with open('intents.json', 'r') as file:
-        intents = json.load(file)
-except FileNotFoundError:
-    print("The file 'intents.json' was not found.")
-    intents = None
-except json.JSONDecodeError as e:
-    print(f"Error decoding JSON: {e}")
-    intents = None
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
-    intents = None
+    def clean_up_sentences(self, sentence):
+        sentence_words = nltk.word_tokenize(sentence)
+        sentence_words = [self.lemmatizer.lemmatize(word) for word in sentence_words]
+        return sentence_words
 
-words = pickle.load(open('words.pkl', 'rb'))
-classes = pickle.load(open('classes.pkl', 'rb'))
-model = load_model('chatbot_model.h5')
+    def bag_of_words(self, sentence):
+        sentence_words = self.clean_up_sentences(sentence)
+        bag = [0] * len(self.words)
+        for w in sentence_words:
+            for i, word in enumerate(self.words):
+                if word == w:
+                    bag[i] = 1
+        return np.array(bag)
 
-def clean_up_sentences(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
-    return sentence_words
+    def predict_class(self, sentence):
+        bow = self.bag_of_words(sentence)
+        res = self.model.predict(np.array([bow]))[0]
+        ERROR_THRESHOLD = 0.25
+        result = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
 
-def bag_of_words(sentence):
-    sentence_words = clean_up_sentences(sentence)
-    bag = [0] * len(words)
-    for w in sentence_words:
-        for i, word in enumerate(words):
-            if word == w:
-                bag[i] = 1
-    return np.array(bag)
+        result.sort(key=lambda x: x[1], reverse=True)
+        return_list = []
+        for r in result:
+            return_list.append({'intent': self.classes[r[0]], 'probability': str(r[1])})
+        return return_list
 
-def predict_class(sentence):
-    bow = bag_of_words(sentence)
-    res = model.predict(np.array([bow]))[0]
-    ERROR_THRESHOLD = 0.25
-    result = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-
-    result.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in result:
-        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
-    return return_list
-    
-def get_response(intent_list, intent_json):
-    tag = intent_list[0]['intent']
-    list_of_intents = intent_json['intents']
-    for intent in list_of_intents:
-        if intent['tags'] == tag:
-            result = random.choice(intent['responses'])
-            break
-    return result
+    def get_response(self, intent_list):
+        tag = intent_list[0]['intent']
+        list_of_intents = self.intents['intents']
+        for intent in list_of_intents:
+            if intent['tags'] == tag:
+                result = random.choice(intent['responses'])
+                break
+        return result
 
 class ChatGUI:
-    def __init__(self, master):
+    def __init__(self, master, chatbot):
         self.master = master
         self.master.title("Chatbot GUI")
+        self.chatbot = chatbot
 
         self.chat_log = Text(master, state='disabled', wrap='word', font=("Arial", 12))
         self.chat_log.pack(expand=True, fill=tk.BOTH)
@@ -85,8 +87,8 @@ class ChatGUI:
         self.update_chat_log(f"You: {user_message}")
 
         # Get the chatbot's response
-        intent_list = predict_class(user_message)
-        bot_response = get_response(intent_list, intents)
+        intent_list = self.chatbot.predict_class(user_message)
+        bot_response = self.chatbot.get_response(intent_list)
 
         self.update_chat_log(f"Chatbot: {bot_response}")
 
@@ -98,5 +100,6 @@ class ChatGUI:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    chat_gui = ChatGUI(root)
+    chatbot_instance = Chatbot()
+    chat_gui = ChatGUI(root, chatbot_instance)
     root.mainloop()
--- My Heart Pal/dashboard.py
+++ My Heart Pal/dashboard.py
@@ -1,27 +1,47 @@
 # dashboard.py
 import tkinter as tk
+from tkinter import messagebox
+from chatbot import ChatGUI, Chatbot
 
 class DashboardScreen(tk.Tk):
     def __init__(self, home_screen):
         super().__init__()
         self.title("Dashboard")
-        self.geometry("300x150")
+        self.geometry("400x200")
         self.state('zoomed')  # Maximize the window
-        self.resizable(False, False)  # Set resizable to False for both width and height
+        self.resizable(False, False)
 
         self.label = tk.Label(self, text="Welcome to Dashboard", font=("Helvetica", 14))
         self.label.pack(pady=10)
 
+        self.chatbot_instance = Chatbot()
+
+        self.chat_button = tk.Button(self, text="Chat With AI", command=self.open_chat)
+        self.chat_button.pack(pady=5)
+
         self.logout_button = tk.Button(self, text="Logout", command=self.logout)
         self.logout_button.pack(pady=5)
 
+        # Store a reference to the home screen to go back after logout
         self.home_screen = home_screen
 
+    def open_chat(self):
+        # Placeholder for opening chatbot.py
+        self.withdraw()
+        chat_gui = ChatGUI(tk.Tk(), self.chatbot_instance)
+        chat_gui.master.protocol("WM_DELETE_WINDOW", lambda: self.on_chat_close(chat_gui))
+
+    def on_chat_close(self, chat_gui):
+        chat_gui.master.destroy()
+        self.deiconify()
+
     def logout(self):
+        # Close current window and go back to home screen
         self.withdraw()
         self.home_screen.deiconify()
 
 if __name__ == "__main__":
-    home_screen = home.HomeScreen()
+    # For testing, pass a dummy HomeScreen instance
+    home_screen = tk.Tk()  # Pass a dummy instance for testing
     app = DashboardScreen(home_screen)
     app.mainloop()
