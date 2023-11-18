800869ea214892446ac799ce42d08d042e2b6468
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-11-18 21:01:28 +0530

    Dashboard Modified

--- My Heart Pal/dashboard.py
+++ My Heart Pal/dashboard.py
@@ -2,13 +2,15 @@
 import tkinter as tk
 from tkinter import messagebox
 from chatbot import ChatGUI, Chatbot
+import quiz
+import analysis
 
 class DashboardScreen(tk.Tk):
     def __init__(self, home_screen):
         super().__init__()
         self.title("Dashboard")
         self.geometry("400x200")
-        self.state('zoomed')  # Maximize the window
+        self.state('zoomed')
         self.resizable(False, False)
 
         self.label = tk.Label(self, text="Welcome to Dashboard", font=("Helvetica", 14))
@@ -18,6 +20,12 @@ class DashboardScreen(tk.Tk):
 
         self.chat_button = tk.Button(self, text="Chat With AI", command=self.open_chat)
         self.chat_button.pack(pady=5)
+
+        self.quiz_button = tk.Button(self, text="Play quiz to know your heart better", command=self.open_quiz)
+        self.quiz_button.pack(pady=5)
+
+        self.analysis_button = tk.Button(self, text="Analysis of Heart by ML", command=self.open_analysis)
+        self.analysis_button.pack(pady=5)
 
         self.logout_button = tk.Button(self, text="Logout", command=self.logout)
         self.logout_button.pack(pady=5)
@@ -31,8 +39,28 @@ class DashboardScreen(tk.Tk):
         chat_gui = ChatGUI(tk.Tk(), self.chatbot_instance)
         chat_gui.master.protocol("WM_DELETE_WINDOW", lambda: self.on_chat_close(chat_gui))
 
+    def open_quiz(self):
+        # Placeholder for opening quiz.py
+        self.withdraw()
+        quiz_app = quiz.QuizApp(self)
+        quiz_app.protocol("WM_DELETE_WINDOW", lambda: self.on_quiz_close(quiz_app))
+
+    def open_analysis(self):
+        # Placeholder for opening analysis.py
+        self.withdraw()
+        analysis_app = analysis.AnalysisApp(self)
+        analysis_app.protocol("WM_DELETE_WINDOW", lambda: self.on_analysis_close(analysis_app))
+
     def on_chat_close(self, chat_gui):
         chat_gui.master.destroy()
+        self.deiconify()
+
+    def on_quiz_close(self, quiz_app):
+        quiz_app.destroy()
+        self.deiconify()
+
+    def on_analysis_close(self, analysis_app):
+        analysis_app.destroy()
         self.deiconify()
 
     def logout(self):
