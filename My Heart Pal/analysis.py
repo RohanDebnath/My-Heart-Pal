7b890dffb29c3fb403fca7f2dd38344f4e04f0dc
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-02-10 17:28:36 +0530

    Combobox label done

--- My Heart Pal/analysis.py
+++ My Heart Pal/analysis.py
@@ -126,4 +126,12 @@ R5.place(x=387, y=10)
 R6 = Radiobutton(Detail_entry, text="No", variable=exang, value=2, command=selection3)
 R6.place(x=430, y=10)
 
+#ComboBox
+Label(Detail_entry,text="cp:",font="arial 13",width=8,bg=framebg,fg=framefg).place(x=10,y=50)
+Label(Detail_entry,text="restecg:",font="arial 13",width=8,bg=framebg,fg=framefg).place(x=10,y=90)
+Label(Detail_entry,text="slope:",font="arial 13",width=8,bg=framebg,fg=framefg).place(x=10,y=130)
+Label(Detail_entry,text="ca:",font="arial 13",width=8,bg=framebg,fg=framefg).place(x=10,y=170)
+Label(Detail_entry,text="thal:",font="arial 13",width=8,bg=framebg,fg=framefg).place(x=10,y=210)
+
+
 root.mainloop()
