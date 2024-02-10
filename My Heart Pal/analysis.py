6c836cdc20f2572b60987d991e5e2360debe3886
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-02-11 01:46:19 +0530

    Info working done

--- My Heart Pal/analysis.py
+++ My Heart Pal/analysis.py
@@ -21,8 +21,37 @@ root=Tk()
 root.title("Heart Attack Prediction System Using Machine Learning")
 root.geometry("1450x730+60+80")
 root.state('zoomed')
-root.resizable(False,False)
+# root.resizable(False,False)
 root.config(bg=background)
+#Opening info window by button 
+def Info():
+    Icon_window=Toplevel(root)
+    Icon_window.title("Info")
+    Icon_window.geometry("700x600+400+100")
+
+    #Icon Setting
+    icon_Image=PhotoImage(file="D:\Project\Python AI Projects\My Heart Pal\info-icon.png")
+    Icon_window.iconphoto(False,icon_Image)
+
+    #Heading
+    Label(Icon_window,text="Information Related to Dataset",font="robot 17 bold").pack(padx=20,pady=20)
+
+    #info
+    Label(Icon_window,text="age - age in years",font="arial 11").place(x=20,y=100)
+    Label(Icon_window,text="sex - sex(1=male; 0=female)",font="arial 11").place(x=20,y=130)
+    Label(Icon_window,text="cp - chest pain type (0 = typical angina; 1 = atypical angina; 2 = non-angial pain; 3 = asymptomatic)",font="arial 11").place(x=20,y=160)
+    Label(Icon_window,text="trestbps - resting blood pressure (in mm Hg on admission to the hospital)",font="arial 11").place(x=20,y=190)
+    Label(Icon_window,text="chol - serum cholestoral in mg/dl",font="arial 11").place(x=20,y=220)
+    Label(Icon_window,text="fbs - fasting blood sugar > 120 mg/dl (1 = true; 0 = false)",font="arial 11").place(x=20,y=250)
+    Label(Icon_window,text="restecg - resting electrocardiographic result (0 = normal; 1 = having ST-T; 2 = hypertrophy)",font="arial 11").place(x=20,y=280)
+    Label(Icon_window,text="thalach - maximum heart rate achieved",font="arial 11").place(x=20,y=310)
+    Label(Icon_window,text="exang - exercise include angina (1 = yes; 0 = no)",font="arial 11").place(x=20,y=340)
+    Label(Icon_window,text="oldpeak - ST depression included by exercise relative to rest",font="arial 11").place(x=20,y=370)
+    Label(Icon_window,text="slope - the slope of the peak exercise ST segment (0= upsloping: 1 = flat; 2 = downsloping)",font="arial 11").place(x=20,y=400)
+    Label(Icon_window,text="ca - number of major vessels (0-3) colored by flourospy",font="arial 11").place(x=20,y=430)
+    Label(Icon_window,text="thal - 0 - normal; 1 = fixed defect ; 2 = reversable defect",font="arial 11").place(x=20,y=460)
+
+    Icon_window.mainloop()
 
 #Logo portion
 img_icon = PhotoImage(file="D:\Project\Python AI Projects\My Heart Pal\icon.png")
@@ -214,7 +243,7 @@ Button(root,image=analysis_button,bg="#FDD0D3",border=0,cursor="hand2").place(x=
 
 #Info Button
 info_button=PhotoImage(file="D:\Project\Python AI Projects\My Heart Pal\info-icon.png")
-Button(root,image=info_button,bd=0,bg=background,cursor="hand2").place(x=1450,y=390)
+Button(root,image=info_button,bd=0,bg=background,cursor="hand2",command=Info).place(x=1450,y=390)
 
 #save  button
 save_btn=PhotoImage(file="D:\Project\Python AI Projects\My Heart Pal\save button.png")
--- My Heart Pal/tempCodeRunnerFile.py
+++ My Heart Pal/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-Learning
+root.resizable(False,False)
